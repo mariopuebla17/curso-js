a65e372f457211186476a37bba5a91e1d1d24fe8
Mario Puebla Junior <mariopuebla17@gmail.com>
2023-03-15 11:14:12 -0300

    conhecimentos adquiridos até aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
